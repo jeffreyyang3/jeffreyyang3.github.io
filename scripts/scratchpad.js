dd5cb033c96f8afdcb1ee4869fc47c369b7556ef
JeffreyYang <jeffreyhasmail@gmail.com>
2018-01-07 21:36:15 -0800

    color

--- scripts/scratchpad.js
+++ scripts/scratchpad.js
@@ -7,7 +7,8 @@ arry = ["asdf", 42, true];
 
 alert("...Ready for It?");
 var h1 = document.querySelector("h1");
-var body = document.querySelector("body");
+var body = document.body;
+
 
 
 
@@ -16,8 +17,8 @@ var body = document.querySelector("body");
 function dank(){
 	 i = prompt("enter a number 2 add 2");
   	 document.getElementById("jstest").innerHTML = "RESULT: " + (Number(i) + 2);
-  	 body.style.background = "red";
-	 h1.style.color = "pink";
+  	 document.body.style.background = "red";
+	 //h1.style.color = "pink";
 }
 function change(){
 	h1.style.color = "pink";
