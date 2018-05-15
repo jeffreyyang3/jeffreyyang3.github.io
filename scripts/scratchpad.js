1a602b9b4c273b7a4b0d3f9a933911bbdff41a36
JeffreyYang <jeffreyhasmail@gmail.com>
2018-05-14 20:07:49 -0700

    more interactivity

--- scripts/scratchpad.js
+++ scripts/scratchpad.js
@@ -17,9 +17,20 @@ arry = ["asdf", 42, true];
 function dank(){
 	 i = prompt("enter a number 2 add 2");
   	 document.getElementById("jstest").innerHTML = "RESULT: " + (Number(i) + 2);
-  	 document.body.style.background = "purple";
+  	 document.body.style.backgroundColor = "purple";
 
   	 
 	 
+}
+function change(){
+	document.body.style.backgroundColor = getRandomColor();
+}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
